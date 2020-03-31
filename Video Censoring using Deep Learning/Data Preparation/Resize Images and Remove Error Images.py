db2dcff2291cf67372e31c8c8a08981df7c20fe7
amittal-tcd <amittal@tcd.ie>
2020-03-31 01:56:06 +0100

    AI Project Update

--- Video Censoring using Deep Learning/Data Preparation/Resize Images and Remove Error Images.py
+++ Video Censoring using Deep Learning/Data Preparation/Resize Images and Remove Error Images.py
@@ -4,6 +4,7 @@ import os
 from multiprocessing import Pool
 import random
 import datetime
+import shutil
 
 # defining function to resize
 def resize(name):
@@ -30,8 +31,16 @@ typ = '0' # For non porn images
 
 # Creating list of files in the folder
 l = os.listdir(src)
-l = [i for i in l if i[:-3].lower == 'jpeg']
+l = [i for i in l if (i[-4:].lower() == 'jpeg') or (i[-3:].lower() == 'jpg')]
 print('Number of images in non porn = ',len(l))
+
+# Deleting previous runs and creating fresh folders
+shutil.rmtree(dest + 'test')
+shutil.rmtree(dest + 'train')
+shutil.rmtree(dest + 'val')
+os.mkdir(dest + 'test')
+os.mkdir(dest + 'train')
+os.mkdir(dest + 'val')
 
 # Running pooled processes
 pool = Pool()
@@ -43,7 +52,7 @@ typ = '1' # For Porn image
 
 # Creating list of files in the folder
 l2 = os.listdir(src)
-l2 = [i for i in l2 if i[:-3].lower == 'jpeg']
+l2 = [i for i in l2 if (i[-4:].lower() == 'jpeg') or (i[-3:].lower() == 'jpg')]
 l2 = random.sample(l2,len(l))
 print('Number of images in non porn = ',len(l2))
 
